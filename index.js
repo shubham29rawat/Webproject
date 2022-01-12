979d5c8336e665e6674d59e2feae462140b1f253
shubham rawat <shubham29rawat@gmail.com>
2022-01-12 12:38:53 -0500

    first

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express route
+app.use('/', require('./routes/index'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`error in running the server : ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('require');
+
+const router = express.Router();
+
+console.log("router loaded")
+
+
+module.exports = router;
