b34707ff342e400f5422693348ba2cfc11a5d26a
shubham rawat <shubham29rawat@gmail.com>
2022-01-12 19:14:11 -0500

    add a controlller and a router for home

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('require');
 
 const router = express.Router();
+const homeController = require('../controller/home_controller')
 
 console.log("router loaded")
 
+router.get('/', homeController.home)
 
 module.exports = router;
